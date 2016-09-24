446798af18230e7939683dc22fe0f60c490b53f0
KeilorMoreira <kmoralvarado@gmail.com>
2016-09-23 23:45:00 -0600

    0002 - 23/09/16 - 11:05

--- TestCreator/main.cpp
+++ TestCreator/main.cpp
@@ -2,19 +2,25 @@
 
 using namespace std;
 
+
+//#######################   Listas de datos  #############################
+/*
+TDA examen, lista doble, falta decidir forma de enlace
+con partes(SelecUnica y RespBrebe que son sublistas)
+*/
 struct examen{
     float nota;
     string materia;
     string grupo;
     string parcial;
-    int puntosObtenidos;
+    int puntosObt;
     float porcentje;
     int puntosTotales;
     struct examen*sig,*ant;
-    struct examenParte*exaP;
-    examen(float n,string m,int pO int pT,float p,string g,string par){
+    struct partes*secciones_ptr;
+    examen(float n,string m,int pO,int pT,float p,string g,string par){
         nota=n;
-        puntosObtenidos=pO;
+        puntosObt=pO;
         puntosTotales=pT;
         materia=m;
         porcentje=p;
@@ -22,66 +28,86 @@ struct examen{
         parcial=par;
         sig=NULL;
         ant=NULL;
-        exaP=NULL;
+        secciones_ptr=NULL;
     }
 }*primeroExamen;
-struct pregunta{
-    int puntosOct;
+
+// Parte del examen, lista con preguntas de Seleccion Unica
+struct pregSelecUnic{
+    int puntosObt;
     int puntosPre;
-    string encabesado;
-    struct pregunta*sig;
-    struct preguntaRespuesta*pregResp;
-    pregunta(int pO,int pP,string e){
-        puntosOct=pO;
+    string pregunta; // string con la pregunta
+    struct pregSelecUnic*sig;
+    struct selecUnica *enlaceSel_ptr;
+    pregSelecUnic(int pO,int pP,string p){
+        puntosObt=pO;
         puntosPre=pP;
-        encabesado=e;
+        pregunta=p;
         sig=NULL;
-        pregResp=NULL;
+        enlaceSel_ptr=NULL;
     }
-}*primeroPregunta;
-struct respuesta{
-    string repuetaCor;
-    struct respuesta*sig;
-    respuesta(){
+}*p_PregSel;
+
+// Parte del examen, lista con preguntas de Respuesta Brebe
+struct pregRespBrebe{
+    int puntosObt;
+    int puntosPre;
+    string pregunta; // string con la pregunta
+    struct pregRespBrebe*sig;
+    struct RespBrebe *enlaceRespBre_ptr;
+    pregRespBrebe(int pO,int pP,string p){
+        puntosObt=pO;
+        puntosPre=pP;
+        pregunta=p;
+        sig=NULL;
+        enlaceRespBre_ptr=NULL;
     }
-}*primeroRespuesta;
-struct parte{
-    string encabesado;
-    string tipo;
-    string partes;
+}*p_PregRespBre;
+
+//#######################   Sublistas de enlace  #############################
+/*
+Puntero heterogenio de enlace a examen que tiene 2 tipos de listas
+una para las tipo seleccion y otra para las de respuesta brebe.
+*/
+struct partes{
+    struct selecUnica*L_selecUnica;
+    struct RespBrebe*L_respBrebe;
+    struct partes*sig;
+    partes(){
+    }
+};
+
+// Puntero heterogenio, sublista de pregSelecUnic
+struct selecUnica{
+    string descripcion;
     int puntos;
     int puntosObt;
-    struct partePregunta*parpre;
-    struct parte*sig;
-    parpre(string e,string p,string t,int pu,int pO){
-        encabesado=e;
-        tipo=t;
-        partes=p;
-        puntos=pu;
-        puntosObt=pO;
-        parpre=NULL;
+    struct selecUnica*sig;
+    struct pregSelecUnic *pregSelUni_prt;
+    selecUnica(string des,int pts,int ptsOb){
+        descripcion=des;
+        puntos=pts;
+        puntosObt=ptsOb;
         sig=NULL;
+        pregSelUni_prt=NULL;
     }
-}*primeraParte;
-struct examenParte{
-    struct examenParte*sig,*ant;
-    struct parte*parE;
 };
-struct partePregunta{
-    struct partePregunta*sig,*ant;
-    struct pregunta*preg;
+
+// Puntero heterogenio, sublista de pregRespBrebe
+struct RespBrebe{
+    string descripcion;
+    int puntos;
+    int puntosObt;
+    struct RespBrebe*sig;
+    struct pregRespBrebe*pregRespBre_prt;
+    RespBrebe(string des,int pts,int ptsO){
+        descripcion=des;
+        puntos=pts;
+        puntosObt=ptsO;
+        sig=NULL;
+        pregRespBre_prt=NULL;
+    }
 };
-struct preguntaRespuesta{
-    struct preguntaRespuesta*sig,*ant;
-    struct respuesta*resp;
-};
-void insertarExamen(string m,int pT,string g,string par){
-    struct examen*nExamen;
-}
-
-
-
-
 
 
 
